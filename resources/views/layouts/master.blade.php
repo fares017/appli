7bae7a2be611c33a9217c4264a8ec6bfcf2e27b1
fares017 <fareshb07@gmail.com>
2021-08-24 19:46:05 +0200

    apré la correction des erreurs3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,20 +15,20 @@
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 
 		<!-- Bootstrap -->
-		<link type="text/css" rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}" />
+		<link type="text/css" rel="stylesheet" href="{{ URL::secure_asset('css/bootstrap.min.css') }}" />
 
 		<!-- Slick -->
-		<link type="text/css" rel="stylesheet"   href="{{ url('css/slick.css') }}" />
-		<link type="text/css" rel="stylesheet"  href="{{ url('css/slick-theme.css') }}" />
+		<link type="text/css" rel="stylesheet" href="{{ URL::secure_asset('css/slick.css') }}"  />
+		<link type="text/css" rel="stylesheet" href="{{ URL::secure_asset('css/slick-theme.css') }}"  />
 
 		<!-- nouislider -->
-		<link type="text/css" rel="stylesheet"  href="{{ url('css/nouislider.min.css') }}" />
+		<link type="text/css" rel="stylesheet"  href="{{ URL::secure_asset('css/nouislider.min.css') }}"  />
 
 		<!-- Font Awesome Icon -->
-		<link rel="stylesheet"  href="{{ url('css/font-awesome.min.css') }}" >
+		<link rel="stylesheet" href="{{ URL::secure_asset('css/font-awesome.min.css') }}"   >
 
 		<!-- Custom stlylesheet -->
-		<link type="text/css" rel="stylesheet"  href="{{ url('css/style.css') }}"  />
+		<link type="text/css" rel="stylesheet"  href="{{ URL::secure_asset('css/style.css') }}"   />
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -391,12 +391,12 @@
 		<!-- /FOOTER -->
 
 		<!-- jQuery Plugins -->
-		<script type="text/javascript" src="{{ URL::asset('js/jquery.min.js') }}"></script>
-		<script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-		<script type="text/javascript" src="{{ URL::asset('js/slick.min.js') }}"></script>
-		<script type="text/javascript" src="{{ URL::asset('js/nouislider.min.js') }}"></script>
-		<script type="text/javascript" src="{{ URL::asset('js/jquery.zoom.min.js') }}"></script>
-		<script type="text/javascript" src="{{ URL::asset('js/main.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/jquery.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/bootstrap.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/slick.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/nouislider.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/jquery.zoom.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::secure_asset('js/main.js') }}"></script>
 		
 
         @yield('extra-js')
